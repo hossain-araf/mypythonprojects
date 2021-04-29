0c40b8a462076224b6a933237fc705fd1a1010ed
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-04-29 14:22:09 +0600

    Add files via upload

---
+++ boy calmetree.py
@@ -0,0 +1,10 @@
+def boycal():
+    w=float(input("what is your weight"))
+    h=float(input("what is your currrent height"))
+    a=float(input("hat is your age"))
+
+    bmi=66+(13.7*w)+(5*h)-(6.8*a)
+    print("your daily cal want is "+str(bmi))
+
+
+boycal()
