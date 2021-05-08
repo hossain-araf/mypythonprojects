948a20ea80b25a37705ba9b0a6b58400f8107315
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-05-08 16:56:18 +0600

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,15 @@
+
+
+
+
+operation=input("whats your favourite game football or cricket")
+times= int(input("how many times do you want to play"))
+
+if operation=="cricket" and times<=12:
+    print("please play with me ")
+elif operation=="football" or times>=12:
+    print("i am sorry, i cant play with you now")
+
+
+  
+    
