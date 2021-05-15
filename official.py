c73d66cdd55a5da5ac72d1dca85f2a0ba7089336
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-05-15 22:05:32 +0600

    Add files via upload

---
+++ official.py
@@ -0,0 +1,33 @@
+a,b=0,1
+while a<10:
+    print(a)
+    a,b=b,a+b
+ae,ad=0,1
+while ae<190:
+    print(ae)
+    ae,ad=ad,ae+ad
+2 + 2
+print('"Isn\'t," they said.')
+s = 'First line.\nSecond line.'
+print(s) 
+arafa='do yoy live in bangldesh\n"or you are american"'
+print(arafa)
+monker='donkey'
+print(monker[0])
+print(monker[-3])
+print(monker[3:])
+print(monker[0:3])
+asd='very'+monker[1]
+print(asd)
+akskd='very'+monker[1:]
+print(akskd)
+print(len(akskd))
+print(len(arafa))
+alexa=['banabna','apple','orange','graps']
+print(alexa[1])
+print(alexa[-3:])
+alexa[0]='jackfruot'
+print(alexa)
+son=['intel','amd','snapdragon']
+son[2]='exynos'
+print(son)
