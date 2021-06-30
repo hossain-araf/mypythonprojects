e069cb976f4a51f4f55590323a5a7bc8bef55cc7
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-06-30 16:57:00 +0600

    Add files via upload

---
+++ libraryversion.py
@@ -0,0 +1,21 @@
+# importing pandas as pd
+import pandas as pd
+
+# Check the version
+print(pd.__version__)
+
+
+import seaborn as sns 
+print(sns.__version__)
+
+
+
+import numpy as np
+print(np.__version__)
+
+
+
+
+
+
+
